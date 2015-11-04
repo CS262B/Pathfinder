07bdd8f1d396cf9efdf6b586dd4e53584421eeb0
CRRDerek <1upderek@gmail.com>
2015-11-04 15:56:09 -0500

    Added comments

--- app/src/main/java/edu/calvin/cs262/prototype/DestActivity.java
+++ app/src/main/java/edu/calvin/cs262/prototype/DestActivity.java
@@ -23,8 +23,9 @@ public class DestActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dest);
 
-        //back to the main menu
+        // Initialize back button
         Button btnMenu= (Button) findViewById(R.id.backmenubutton);
+        // Add listener to "Back" button with intent to switch to the main menu
         btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,8 +34,8 @@ public class DestActivity extends Activity{
             }
         });
 
-        //to map activity with eventual display for entered building and room number
         Button btnGo= (Button) findViewById(R.id.goButton);
+        // Add listener to "Go" button with intent to switch to the map activity
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
